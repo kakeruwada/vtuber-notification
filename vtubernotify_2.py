5dfda7404b1b3f88e68ec742a48968f3bc299828
和田翔 <wadakakeru@wadashounoMacBook-Pro.local>
2020-12-28 18:12:26 +0900

    make it better

--- vtubernotify_2.py
+++ vtubernotify_2.py
@@ -11,7 +11,7 @@ from linebot.models import (
 )
 import ytResponse
 import pprint
-from flask import Flask, request, abort
+from flask import Flask, jsonify, abort
 
 channel_access_token = ('gdwFD3CEbW2keHoILCal0xC9nTEy4PdLfLEmqnai2w8N1x8Gcy24EhAfSFh7m8MMesD1/d7e+OmblqrazFVQiLbEwE55eBYcy64QW9n52CkfyUX4NFsl4t6AC4kRz4IEOdosSS/pAQtCI4Kq14rAHgdB04t89/1O/w1cDnyilFU=')
 
@@ -19,32 +19,23 @@ line_bot_api = LineBotApi(channel_access_token)
 
 app = Flask(__name__)
 
-@app.route("/callback", methods = ['POST'])
+@app.route('/callback')
 def callback():
-    # リクエストヘッダーから署名検証のための値を取得
-    signature = request.headers['X-Line-Signature']
-
-    # リクエストボディを取得
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
+    return abort(200)
 
 q1 = 'ホロライブ切り抜き　OR　ホロライブ手描き'
 q2 = 'にじさんじ切り抜き OR にじさんじ手描き OR にじさんじ漫画'
 
-qlist = []
-qlist.extend([q2, q1])
+#qlist = [q1]
+#qlist.extend([q2, q1])
+Response = ytResponse.ytResponse().ytResponse(q1)
+listedRes = list(Response.items())
+#クエリを指定して検索結果を取得
 
-for q in qlist:
-    Res = ytResponse.ytResponse().ytResponse(q)
-    #クエリを指定して検索結果を取得
-    pResList = pprint.pformat(list(Res.items()))
+#検索結果を1動画ずつ出力（LINEメッセージにて見やすくするため）
+for R in range(len(Res)):
+    item = listedRes[R]
+    pResList = pprint.pformat(item)
     #辞書形式からリストに変更しpprintで見やすくする
     DeResList = str(pResList).decode('string-escape')
     #デコード処理
