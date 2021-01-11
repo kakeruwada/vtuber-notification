a9cd620943ab33489e248c753d5d7b461914900f
和田翔 <wadakakeru@wadashounoMacBook-Pro.local>
2021-01-11 17:43:27 +0900

    ユーザー情報を取得するprofileを追加

--- app.py
+++ app.py
@@ -15,34 +15,6 @@ import pprint
 from flask import Flask, request, abort
 import datetime
 
-q1 = 'ホロライブ切り抜き　OR　ホロライブ手描き'
-q2 = 'にじさんじ切り抜き OR にじさんじ手描き OR にじさんじ漫画'
-#youtubeAPIに送信するクエリを設定
-
-#クエリに対する検索結果をLINEに送信
-def sendYTresult():
-    dt = datetime.datetime.now()
-    if dt.hour < 12:
-        Response = ytResponse.ytResponse().ytResponse(q1)
-    else:
-        Response = ytResponse.ytResponse().ytResponse(q2)
-    #午前はホロライブ、午後はにじさんじの切り抜きを通知
-
-    listedRes = list(Response.items())
-    #クエリを指定して検索結果を取得
-
-    #検索結果を1動画ずつ出力（LINEメッセージにて見やすくするため）
-    for R in range(len(listedRes)):
-        item = listedRes[R]
-        pResList = pprint.pformat(item)
-        #辞書形式からリストに変更しpprintで見やすくする
-        DeResList = pResList
-        #デコード処理
-        try:
-            line_bot_api.push_message('Uf0f5062854847968101f84a27657f739', TextSendMessage(text=DeResList))
-        except LineBotApiError:
-           line_bot_api.push_message('Uf0f5062854847968101f84a27657f739', TextSendMessage(text="エラー"))
-
 app = Flask(__name__)
 
 channel_access_token = ('gdwFD3CEbW2keHoILCal0xC9nTEy4PdLfLEmqnai2w8N1x8Gcy24EhAfSFh7m8MMesD1/d7e+OmblqrazFVQiLbEwE55eBYcy64QW9n52CkfyUX4NFsl4t6AC4kRz4IEOdosSS/pAQtCI4Kq14rAHgdB04t89/1O/w1cDnyilFU=')
@@ -68,9 +40,38 @@ def callback():
 #LINEアカウントがフォローされた時にメッセージを送信
 @handler.add(FollowEvent)
 def handle_follow(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="過去一日の切り抜きをそれぞれ朝9時（ホロライブ）と夕方18時（にじさんじ）に10件ずつ送ります！\n-------------\nURLタップでアプリ内ののブラウザに遷移、サムネイルタップでLINEアプリ内のプレイヤーで視聴します\n-------------\nLINEの「設定」より「LINE Labs」、「リンクをSafariで開く」をオンにすると、URLタップ時にSafariまたはYoutubeアプリで視聴できます（iOSのみ）"))
+
+q1 = 'ホロライブ切り抜き　OR　ホロライブ手描き'
+q2 = 'にじさんじ切り抜き OR にじさんじ手描き OR にじさんじ漫画'
+#youtubeAPIに送信するクエリを設定
+
+#クエリに対する検索結果をLINEに送信
+def sendYTresult():
+    dt = datetime.datetime.now()
+    if dt.hour < 12:
+        Response = ytResponse.ytResponse().ytResponse(q1)
+    else:
+        Response = ytResponse.ytResponse().ytResponse(q2)
+    #午前はホロライブ、午後はにじさんじの切り抜きを通知
+
+    listedRes = list(Response.items())
+    #クエリを指定して検索結果を取得
+
+    #検索結果を1動画ずつ出力（LINEメッセージにて見やすくするため）
+    for R in range(len(listedRes)):
+        item = listedRes[R]
+        pResList = pprint.pformat(item)
+        #辞書形式からリストに変更しpprintで見やすくする
+        DeResList = pResList
+        #デコード処理
+        try:
+            line_bot_api.push_message(to=profile.user_id, TextSendMessage(text=DeResList))
+        except LineBotApiError:
+           line_bot_api.push_message(to=profile.user_id, TextSendMessage(text="エラー"))
 
 
 if __name__ == "__main__":
