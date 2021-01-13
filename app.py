7d7ef07f5753024e8603594f9ab8c4022d3399df
和田翔 <wadakakeru@wadashounoMacBook-Pro.local>
2021-01-14 08:23:02 +0900

    change dt.hour from 3 to 0

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ q2 = 'にじさんじ切り抜き OR にじさんじ手描き OR にじさんじ
 #クエリに対する検索結果をLINEに送信
 def sendYTresult():
     dt = datetime.datetime.now()
-    if dt.hour < 3: #JST時間で12:00以降か以前か判定（UTC時間への補正のため+9時間）
+    if dt.hour < 0: #JST時間で9:00以降か以前か判定（UTC時間への補正のため+9時間）
         Response = ytResponse.ytResponse().ytResponse(q1)
     else:
         Response = ytResponse.ytResponse().ytResponse(q2)
