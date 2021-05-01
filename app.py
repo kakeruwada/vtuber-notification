021a05cea7ee93d4faf75b2d1c6973687bc1a06c
和田翔 <wadakakeru@wadashounoMacBook-Pro.local>
2021-05-01 18:04:48 +0900

    create_ifnotexists,get_response_message関数を追加、handle_message()関数追加send_yt_result()にwith句〜sys.exit句まで追加変更

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
+import psycopg2
 import sys
 from linebot import (
     LineBotApi, WebhookHandler
@@ -16,11 +17,16 @@ from flask import Flask, request, abort
 import ytResponse
 import pprint
 import datetime
+import psycopg2
+from psycopg2.extras import DictCursor
+
+#--環境変数系
 
 app = Flask(__name__)
 
 channel_access_token = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 channel_secret = os.environ['YOUR_CHANNEL_SECRET']
+database_url = os.environ.get('DATABASE_URL')
 
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
@@ -32,8 +38,59 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-query1 = 'ホロライブ切り抜き　OR　ホロライブ手描き'
-query2 = 'にじさんじ切り抜き OR にじさんじ手描き OR にじさんじ漫画'
+#--データベース系の関数
+
+def create_ifnotexist():
+    sql = """
+    CREATE TABLE IF NOT EXISTS query.table (
+        id INTEGER,
+        name VARCHAR(20)
+    );
+    """
+    return conn.execute(sql)
+
+def get_response_message(num,line_mess):
+    sql_isert = '"INSERT INTO query.table VALUES('+ num +','+ line_mess +')"'
+    with conn.cursor(cursor_factory=DictCursor) as cur:
+        cur.execute(sql_isert)
+
+#--LINEメッセージ系
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_mssg = event.message.text
+    if  "登録1" in line_mssg:
+        splt = line_mssg.split(":")
+        srch_wrd = splt[1]
+
+        with psycopg2.connect(database_url) as conn:
+            create_ifnotexist()
+            get_response_message(1,srch_wrd)
+
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text= srch_wrd + "を登録したよ！"))
+
+    elif "登録2" in line_mssg:
+        splt = line_mssg.split(":")
+        srch_wrd = splt[1]
+
+        with psycopg2.connect(database_url) as conn:
+            create_ifnotexist()
+            get_response_message(2,srch_wrd)
+
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text= srch_wrd + "を登録したよ！"))
+
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="にゃーん"))
+
 
 #LINEAPIからのHTTPリクエストの署名を検証し、問題ない場合任意の関数を実行
 @app.route("/callback", methods=['POST'])
@@ -57,36 +114,38 @@ def callback():
 def handle_follow(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="過去一日の切り抜きをそれぞれ朝8時（にじさんじ）と朝10時（ホロライブ）に10件ずつ送ります！\n-------------\nURLタップでアプリ内ののブラウザに遷移、サムネイルタップでLINEアプリ内のプレイヤーで視聴します\n-------------\nLINEの「設定」より「LINE Labs」、「リンクをSafariで開く」をオンにすると、URLタップ時にSafariまたはYoutubeアプリで視聴できます（iOSのみ）"))
+        TextSendMessage(text="過去一日の動画をそれぞれ朝8時と朝10時に10件ずつ送ります！\n-------------\nURLタップでアプリ内ののブラウザに遷移、サムネイルタップでLINEアプリ内のプレイヤーで視聴します\n-------------\nLINEの「設定」より「LINE Labs」、「リンクをSafariで開く」をオンにすると、URLタップ時にSafariまたはYoutubeアプリで視聴できます（iOSのみ）\n-------------\n検索ワードに設定したいワードは「登録:〇〇（任意のワード）」のフォーマットで登録:は半角　"))
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+
 
     #クエリに対する検索結果をLINEに送信
-def send_yt_result(q1,q2):
-    dt = datetime.datetime.now()
-    if dt.hour < 12:
-        Response = ytResponse.ytResponse().ytResponse(q1)
-        #JST時間で9:00~21:00はq2の検索結果
-    else:
-        Response = ytResponse.ytResponse().ytResponse(q2)
-        #JST時間で21:00~9:00はq2の検索結果
+def send_yt_result():
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
+            q1 = cur.execute("SELECT value FROM query.table WHERE id = 1")
+            q2 = cur.execute("SELECT value FROM query.table WHERE id = 2")
+            dt = datetime.datetime.now()
+            if dt.hour < 12 and q1 != null:
+                Response = ytResponse.ytResponse().ytResponse(q1)
+                #JST時間で9:00~21:00はq2の検索結果
+            elif q2 != null:
+                Response = ytResponse.ytResponse().ytResponse(q2)
+                #JST時間で21:00~9:00はq2の検索結果
+            else:
+                sys.exit()
 
-    listed_res = list(Response.items())
-    #クエリを指定して検索結果を取得
+            listed_res = list(Response.items())
+            #クエリを指定して検索結果を取得
 
-    #検索結果を1動画ずつ出力（LINEメッセージにて見やすくするため）
-    for r in range(len(listed_res)):
-        item = listed_res[r]
-        pp_response = pprint.pformat(item)
-        #辞書形式からリストに変更しpprintで見やすくする
-        try:
-            line_bot_api.push_message("Uf0f5062854847968101f84a27657f739", TextSendMessage(text=pp_response))
-        except LineBotApiError:
-            line_bot_api.push_message("Uf0f5062854847968101f84a27657f739", TextSendMessage(text="エラーが発生しました"))
+            #検索結果を1動画ずつ出力（LINEメッセージにて見やすくするため）
+            for r in range(len(listed_res)):
+                item = listed_res[r]
+                pp_response = pprint.pformat(item)
+                #辞書形式からリストに変更しpprintで見やすくする
+                try:
+                    line_bot_api.push_message("Uf0f5062854847968101f84a27657f739", TextSendMessage(text=pp_response))
+                except LineBotApiError:
+                    line_bot_api.push_message("Uf0f5062854847968101f84a27657f739", TextSendMessage(text="エラーが発生しました"))
 
 
 if __name__ == "__main__":
