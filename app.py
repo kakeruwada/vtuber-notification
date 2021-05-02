66168c1d21a1373a4bb86b3bcfc3d14ae961493a
和田翔 <wadakakeru@wadashounoMacBook-Pro.local>
2021-05-03 00:55:55 +0900

    minor change

--- app.py
+++ app.py
@@ -49,9 +49,9 @@ def get_response_message(num,line_mess):
                 name VARCHAR(20)
             );
             """
-            sql_isert = '"INSERT INTO query_table(id, name) VALUES("'+ str(num) +'","'+ line_mess +'") ON DUPLICATE KEY UPDATE(name = "'+ line_mess +'")"'
+            sql_isert = '"INSERT INTO query_table(id, name) VALUES("'+ str(num) +'","'+ line_mess +'") ON DUPLICATE KEY UPDATE name = "'+ line_mess +'";"'
 
-            sql_update = '"UPDATE query_table SET name='+ line_mess +' WHERE id='+ str(num) +'"'
+            sql_update = '"UPDATE query_table SET name='+ line_mess +' WHERE id='+ str(num) +';"'
 
             cur.execute(sql)#if not条件付きでテーブルを作る
 
